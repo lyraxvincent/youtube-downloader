4ad0476c09dfa3906880143683b545cd01d2cdd7
lyraxvincent <njongevincent@gmail.com>
2020-05-25 16:16:24 +0300

    Fixed video download without sound

--- youtubeDownloader.py
+++ youtubeDownloader.py
@@ -7,15 +7,13 @@ url = input("input video url :")
 #create stream object
 mystream = pafy.new(url)
 
-#videostreams object
-vidstream = mystream.videostreams
 #audiostreams object
 audstream = mystream.audiostreams
 
 select = input('For audio download enter "A", for video download enter "V": ').upper()
 
 if select == 'A':
-    print('The audiostreams are: \n ', [j for j in enumerate(audstream, start=0)])
+    print('The audiostreams are: \n ', [audio for audio in enumerate(audstream, start=0)])
     choice = eval(input('Enter audio number: '))     #instantiate your choice of audio quality
 
     print('Downloading...', audstream[choice])
@@ -23,11 +21,11 @@ if select == 'A':
     print('Download finished.')
 
 elif select == 'V':
-    print('The videostreams are: \n', [j for j in enumerate(vidstream, start=0)])
-    choice = eval(input('Enter video number: '))     #instantiate your choice of video quality
+    print('Available videostreams with sound are: \n', [video for video in enumerate(mystream.streams, start=0)])
+    choice = eval(input('Enter videostream number: '))     #instantiate your choice of video quality
 
-    print('Downloading...', vidstream[choice])
-    vidstream[choice].download(remux_audio=True)                    #download your video
+    print('Downloading...', mystream.streams[choice])
+    mystream.streams[choice].download()                    #download the video
     print('Download finished.')
 
 else:
