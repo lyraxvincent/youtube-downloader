644171b428cf3b3b68da9a783a8835c8a4cb8a2d
lyraxvincent <njongevincent@gmail.com>
2021-11-30 21:23:44 +0300

    replace pafy with pytube

--- youtubeDownloader.py
+++ youtubeDownloader.py
@@ -1,5 +1,3 @@
-import pafy
-import youtube_dl
 from pytube import YouTube # for videos
 import ffmpeg # audio - video merging
 import os
@@ -9,29 +7,24 @@ import shutil # remove non-empty directories
 # url of video
 url = input("input video url: ")
 
-#create stream object
-mystream = pafy.new(url)
-
-#audiostreams object
-audstream = mystream.audiostreams
+# YouTube object
+yt = YouTube(url)
+title = str(yt.title)
 
 select = input('For audio download enter "A", for video download enter "V": ').upper()
 
 if select == 'A':
-    print('The audiostreams are: \n ', [audio for audio in enumerate(audstream, start=0)])
-    choice = eval(input('Enter audio number: '))     #instantiate your choice of audio quality
 
-    print('Downloading...', audstream[choice])
-    audstream[choice].download()                    #download the audio
-    print('Download finished.')
+    audio_streams = yt.streams.filter(only_audio=True)
+    print("Available audio streams: ", [(tag.itag, '[' + tag.abr + ' : ' + tag.audio_codec + ']') for tag in audio_streams])
+    tag_number = input("Enter audio tag number: ")
+    yt.streams.get_by_itag(tag_number).download(filename=title+'.mp3')
 
 elif select == 'V':
 
     if not ('video' in os.listdir('.')) or ('audio' not in os.listdir('.')):
         os.mkdir('video'); os.mkdir('audio')
     
-    yt = YouTube(url)
-    title = str(yt.title)
     vidstreams = yt.streams.filter(adaptive=True, file_extension='mp4').order_by('resolution')
 
     print("Available video streams: ", [(tag.itag, tag.resolution) for tag in vidstreams])
